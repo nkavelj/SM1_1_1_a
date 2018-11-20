9fdd62652b19e4b43497fae98b7ce8707120ee1a
Nikola Kavelj <Nikola Kavelj@KAVELJ-DESKTOP>
2018-11-20 18:13:41 +0100

    New commit.

--- Sum_S1.m
+++ Sum_S1.m
@@ -3,6 +3,7 @@ clc
 close all
 %% Aufgabe 1a)
 disp('Aufgabe 1a)');
+fprintf('\n');
 X=[A1_SystemI(1:5,2),A1_SystemI(1:5,1),A1_SystemI(1:5,3)];
 Z=[A1_SystemII(1:5,2),A1_SystemII(1:5,1),A1_SystemII(1:5,3)];
 disp('x0, y0, z0, ex, ey, ez, m:');
