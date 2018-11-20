6895816425f4437c4a7de6364ed9a95393c482c2
Nikola Kavelj <Nikola Kavelj@KAVELJ-DESKTOP>
2018-11-20 17:12:29 +0100

    Ausgabe zu 1a hinzugefuegt.

--- Sum_S1.m
+++ Sum_S1.m
@@ -1,7 +1,10 @@
 clc 
 close all
-%% Aufgabe 1 a)
+%% Aufgabe 1a)
 % Vorher WerteGenerator.p ausfuehren!
+disp('Aufgabe 1a)');
 X=[A1_SystemI(1:5,2),A1_SystemI(1:5,1),A1_SystemI(1:5,3)];
 Z=[A1_SystemII(1:5,2),A1_SystemII(1:5,1),A1_SystemII(1:5,3)];
-t=trafoparam_3d_12p(X,Z);
+disp('x0, y0, z0, ex, ey, ez, m:');
+p=trafoparam_3d_12p(X,Z)
+%% Aufgabe 1b)
