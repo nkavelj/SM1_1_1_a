43ed072406c7fe12ebfc1ddbc297cf32f6b1ebbf
Nikola Kavelj <Nikola Kavelj@KAVELJ-DESKTOP>
2018-11-23 21:47:55 +0100

    New commit.

--- Sum_S1.m
+++ Sum_S1.m
@@ -31,13 +31,13 @@ fprintf('\n');
 %% Aufgabe 2b)
 disp('Aufgabe 1b)');
 fprintf('\n');
-disp('Parameter fuer Punkt 1, 2, 5:');
+disp('Parameter fuer Punkt 1, 2, 5 (x0, y0, z0, ex, ey, ez, m):');
 X1=[A1_SystemI(1,2) A1_SystemI(1,1) A1_SystemI(1,3);A1_SystemI(2,2) A1_SystemI(2,1) A1_SystemI(2,3);A1_SystemI(5,2) A1_SystemI(5,1) A1_SystemI(5,3)];
 Z1=[A1_SystemII(1,2) A1_SystemII(1,1) A1_SystemII(1,3);A1_SystemII(2,2) A1_SystemII(2,1) A1_SystemII(2,3);A1_SystemII(5,2) A1_SystemII(5,1) A1_SystemII(5,3)];
 p1=trafoparam_3d_7p(X1,Z1,p);
 fprintf('%.4f\n',p1);
 fprintf('\n');
-disp('Parameter fuer Punkt 1, 4, 5, 6:');
+disp('Parameter fuer Punkt 1, 4, 5, 6 (x0, y0, z0, ex, ey, ez, m):');
 X3=[A1_SystemI(1,2) A1_SystemI(1,1) A1_SystemI(1,3);A1_SystemI(4,2) A1_SystemI(4,1) A1_SystemI(4,3);A1_SystemI(5,2) A1_SystemI(5,1) A1_SystemI(5,3);A1_SystemI(6,2) A1_SystemI(6,1) A1_SystemI(6,3)];
 Z3=[A1_SystemII(1,2) A1_SystemII(1,1) A1_SystemII(1,3);A1_SystemII(4,2) A1_SystemII(4,1) A1_SystemII(4,3);A1_SystemII(5,2) A1_SystemII(5,1) A1_SystemII(5,3);A1_SystemII(6,2) A1_SystemII(6,1) A1_SystemII(6,3)];
 p3=trafoparam_3d_7p(X3,Z3,p);
