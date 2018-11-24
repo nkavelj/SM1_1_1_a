06444a168c4d1d9ebfd81c51adad567036c21028
Nikola Kavelj <Nikola Kavelj@KAVELJ-DESKTOP>
2018-11-24 15:57:11 +0100

    Typo in comment corrected.

--- trafo3d.m
+++ trafo3d.m
@@ -1,5 +1,5 @@
 function [Z] = trafo3d(Q,x0,y0,z0,ex,ey,ez,m)
-%TRAFO3D Führt eine Ähnlichkeitstransformation eines Punkten durch
+%TRAFO3D Führt eine Ähnlichkeitstransformation eines Punktes durch
 D = [cos(ey)*cos(ez) sin(ex)*sin(ey)*cos(ez)-cos(ex)*sin(ez) cos(ex)*sin(ey)*cos(ez)+sin(ex)*sin(ez); cos(ey)*sin(ez) sin(ex)*sin(ey)*sin(ez)+cos(ex)*cos(ez) cos(ex)*sin(ey)*sin(ez)-sin(ex)*cos(ez); -sin(ey) sin(ex)*cos(ey) cos(ex)*cos(ey)];
 X0 = [x0;y0;z0];
 %Q wird transponiert, weil er als Zeilenvektor angenommen wird, er aber für
